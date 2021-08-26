be0e5b8612ed7ee7a7838db179c89e3a61535e44
Jim Tittsler <jimt@onjapan.net>
2021-08-26 18:49:07 +0900

    migrate to Astro

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const sidebar = [
+  {
+    text: 'Introduction',
+    link: '', // No leading slash needed, so this links to the homepage
+    children: [
+      { text: 'About', link: 'about' },
+      { text: 'News', link: 'news' },
+    ],
+  },
+];
