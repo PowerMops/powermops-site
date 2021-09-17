006555f4a61e39a7c891dfffd9f33dbc23c20ace
Jim Tittsler <jimt@onjapan.net>
2021-09-17 19:37:31 +0900

    fix broken links

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const sidebar = [
     text: 'Introduction',
     link: '', // No leading slash needed, so this links to the homepage
     children: [
-      { text: 'About', link: 'about' },
-      { text: 'News', link: 'news' },
+      { text: 'About', link: '/about/' },
+      { text: 'News', link: '/news/' },
     ],
   },
   {
